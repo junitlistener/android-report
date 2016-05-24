20b1192464833998cebf9d75dc5c75e8104d0492
dori <dori.waldman@inner-active.com>
2016-05-24 18:52:23 +0300

    init files

---
+++ src/main/java/com/junitlistener/lib/AndroiDWRunner.java
@@ -0,0 +1,208 @@
+package com.tula.lib;
+
+import android.app.Activity;
+import android.app.Instrumentation;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Environment;
+import android.support.test.internal.runner.RunnerArgs;
+import android.support.test.internal.runner.TestExecutor;
+import android.support.test.internal.runner.TestRequest;
+import android.support.test.internal.runner.TestRequestBuilder;
+import android.support.test.internal.runner.listener.ActivityFinisherRunListener;
+import android.support.test.internal.runner.listener.CoverageListener;
+import android.support.test.internal.runner.listener.DelayInjector;
+import android.support.test.internal.runner.listener.InstrumentationResultPrinter;
+import android.support.test.internal.runner.listener.LogRunListener;
+import android.support.test.internal.runner.listener.SuiteAssignmentPrinter;
+import android.support.test.internal.runner.tracker.AnalyticsBasedUsageTracker;
+import android.support.test.internal.runner.tracker.UsageTracker;
+import android.support.test.internal.runner.tracker.UsageTrackerRegistry;
+import android.support.test.runner.MonitoringInstrumentation;
+import android.support.test.runner.lifecycle.ApplicationLifecycleCallback;
+import android.support.test.runner.lifecycle.ApplicationLifecycleMonitorRegistry;
+import android.util.Log;
+
+import org.junit.runner.notification.RunListener;
+
+/**
+ * Created by dw on 3/12/16.
+ * Fork of http://zutubi.com/source/projects/android-junit-report/
+ */
+public class AndroiDWRunner extends MonitoringInstrumentation {
+
+    private static final String LOG_TAG = "AndroiDWRunner";
+
+    private Bundle mArguments;
+    private InstrumentationResultPrinter mInstrumentationResultPrinter = null;
+    private RunnerArgs mRunnerArgs;
+    private JUnitReportListener mListener;
+
+    @Override
+    public void onCreate(Bundle arguments) {
+        super.onCreate(arguments);
+        mArguments = arguments;
+        mRunnerArgs = new RunnerArgs.Builder().fromManifest(this).fromBundle(getArguments()).build();
+        for (ApplicationLifecycleCallback listener : mRunnerArgs.appListeners) {
+            ApplicationLifecycleMonitorRegistry.getInstance().addLifecycleCallback(listener);
+        }
+        start();
+    }
+
+
+    private Bundle getArguments() {
+        return mArguments;
+    }
+
+    InstrumentationResultPrinter getInstrumentationResultPrinter() {
+        return mInstrumentationResultPrinter;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        Bundle results = new Bundle();
+        try {
+            TestExecutor.Builder executorBuilder = new TestExecutor.Builder(this);
+            if (mRunnerArgs.debug) {
+                executorBuilder.setWaitForDebugger(true);
+            }
+
+            addListeners(mRunnerArgs, executorBuilder);
+
+            TestRequest testRequest = buildRequest(mRunnerArgs, getArguments());
+
+            results = executorBuilder.build().execute(testRequest);
+
+        } catch (RuntimeException e) {
+            final String msg = "Fatal exception when running tests";
+            Log.e(LOG_TAG, msg, e);
+            results.putString(Instrumentation.REPORT_KEY_STREAMRESULT, msg + "\n" + Log.getStackTraceString(e));
+        }
+        finish(Activity.RESULT_OK, results);
+    }
+
+
+    // This method send broadcast message to another application on the device , I use it to know when test finish for the target package
+    private void send(){
+        Intent intent = new Intent();
+        intent.setAction("com.tula.test.finished");
+        intent.putExtra("package", mArguments.getString("package"));
+        getContext().sendBroadcast(intent);
+    }
+
+
+    @Override
+    public void finish(int resultCode, Bundle results) {
+        try {
+            if (mListener != null) {
+                mListener.close();
+            }
+            UsageTrackerRegistry.getInstance().trackUsage("AndroiDWRunner");
+            UsageTrackerRegistry.getInstance().sendUsages();
+        } catch (RuntimeException re) {
+            Log.w(LOG_TAG, "Failed to send analytics.", re);
+        }
+        send();
+        super.finish(resultCode, results);
+    }
+
+    private void addListeners(RunnerArgs args, TestExecutor.Builder builder) {
+        if (args.suiteAssignment) {
+            builder.addRunListener(new SuiteAssignmentPrinter());
+        } else {
+            builder.addRunListener(new LogRunListener());
+            mInstrumentationResultPrinter = new InstrumentationResultPrinter();
+            builder.addRunListener(mInstrumentationResultPrinter);
+            builder.addRunListener(new ActivityFinisherRunListener(this, new MonitoringInstrumentation.ActivityFinisher()));
+
+            // I used external folder to save the results
+            this.mListener = new JUnitReportListener(getTargetContext(),
+                    "junit.xml",
+                    Environment.getExternalStorageDirectory().getAbsolutePath() + "/test_results/",
+                    false,
+                    true);
+
+            builder.addRunListener(this.mListener);
+            addDelayListener(args, builder);
+            addCoverageListener(args, builder);
+        }
+
+        addListenersFromArg(args, builder);
+    }
+
+    private void addCoverageListener(RunnerArgs args, TestExecutor.Builder builder) {
+        if (args.codeCoverage) {
+            builder.addRunListener(new CoverageListener(args.codeCoveragePath));
+        }
+    }
+
+    /**
+     * Sets up listener to inject a delay between each test, if specified.
+     */
+    private void addDelayListener(RunnerArgs args, TestExecutor.Builder builder) {
+        if (args.delayInMillis > 0) {
+            builder.addRunListener(new DelayInjector(args.delayInMillis));
+        } else if (args.logOnly && Build.VERSION.SDK_INT < 16) {
+            // On older platforms, collecting tests can fail for large volume of tests.
+            // Insert a small delay between each test to prevent this
+            builder.addRunListener(new DelayInjector(15 /* msec */));
+        }
+    }
+
+    private void addListenersFromArg(RunnerArgs args, TestExecutor.Builder builder) {
+        for (RunListener listener : args.listeners) {
+            builder.addRunListener(listener);
+        }
+    }
+
+    @Override
+    public boolean onException(Object obj, Throwable e) {
+        InstrumentationResultPrinter instResultPrinter = getInstrumentationResultPrinter();
+        if (instResultPrinter != null) {
+            // report better error message back to Instrumentation results.
+            instResultPrinter.reportProcessCrash(e);
+        }
+        return super.onException(obj, e);
+    }
+
+    /**
+     * Builds a {@link TestRequest} based on given input arguments.
+     * <p/>
+     */
+    // Visible for testing
+    TestRequest buildRequest(RunnerArgs runnerArgs, Bundle bundleArgs) {
+
+        TestRequestBuilder builder = createTestRequestBuilder(this, bundleArgs);
+
+        // only scan for tests for current apk aka testContext
+        // Note that this represents a change from InstrumentationTestRunner where
+        // getTargetContext().getPackageCodePath() aka app under test was also scanned
+        builder.addApkToScan(getContext().getPackageCodePath());
+
+        builder.addFromRunnerArgs(runnerArgs);
+
+        if (!runnerArgs.disableAnalytics) {
+            if (null != getTargetContext()) {
+                UsageTracker tracker = new AnalyticsBasedUsageTracker.Builder(
+                        getTargetContext()).buildIfPossible();
+
+                if (null != tracker) {
+                    UsageTrackerRegistry.registerInstance(tracker);
+                }
+            }
+        }
+
+        return builder.build();
+    }
+
+    /**
+     * Factory method for {@link TestRequestBuilder}.
+     */
+    // Visible for testing
+    TestRequestBuilder createTestRequestBuilder(Instrumentation instr, Bundle arguments) {
+        return new TestRequestBuilder(instr, arguments);
+    }
+}
---
+++ src/main/java/com/junitlistener/lib/JUnitReportListener.java
@@ -0,0 +1,286 @@
+package com.tula.lib;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Environment;
+import android.support.test.internal.runner.listener.InstrumentationRunListener;
+import android.util.Log;
+import android.util.Xml;
+
+import junit.framework.AssertionFailedError;
+import junit.framework.Test;
+import junit.framework.TestCase;
+
+import org.junit.runner.Description;
+import org.junit.runner.notification.Failure;
+import org.xmlpull.v1.XmlSerializer;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.io.Writer;
+import java.util.Locale;
+
+/**
+ * Created by dw on 3/11/16.
+ * Fork of http://zutubi.com/source/projects/android-junit-report/
+ */
+public class JUnitReportListener extends InstrumentationRunListener {
+    private static final String LOG_TAG = "JUnitReportListener";
+
+        private static final String ENCODING_UTF_8 = "utf-8";
+        public static final String TOKEN_SUITE = "__suite__";
+        private static final String TAG_SUITES = "testsuites";
+        private static final String TAG_SUITE = "testsuite";
+        private static final String TAG_CASE = "testcase";
+        private static final String TAG_FAILURE = "failure";
+        private static final String ATTRIBUTE_NAME = "name";
+        private static final String ATTRIBUTE_CLASS = "classname";
+        private static final String ATTRIBUTE_TYPE = "type";
+        private static final String ATTRIBUTE_MESSAGE = "message";
+        private static final String ATTRIBUTE_TIME = "time";
+
+        private static final String[] DEFAULT_TRACE_FILTERS = new String[] {
+                "junit.framework.TestCase", "junit.framework.TestResult",
+                "junit.framework.TestSuite",
+                "junit.framework.Assert.", // don't filter AssertionFailure
+                "java.lang.reflect.Method.invoke(", "sun.reflect.",
+                // JUnit 4 support:
+                "org.junit.", "junit.framework.JUnit4TestAdapter", " more",
+                // Added for Android
+                "android.test.", "android.app.Instrumentation",
+                "java.lang.reflect.Method.invokeNative",
+        };
+        private Context mTargetContext;
+        private String mReportFile;
+        private String mReportDir;
+        private boolean mFilterTraces;
+        private boolean mMultiFile;
+        private FileOutputStream mOutputStream;
+        private XmlSerializer mSerializer;
+        private String mCurrentSuite;
+        private boolean mTimeAlreadyWritten = false;
+        private long mTestStartTime;
+
+
+        public JUnitReportListener( Context targetContext,
+                                    String reportFile,
+                                    String reportDir,
+                                    boolean filterTraces,
+                                    boolean multiFile)
+        {
+            Log.i(LOG_TAG, "Listener created with arguments:\n  report file  : '" + reportFile + "'\n" + "  report dir   : '" + reportDir + "'\n" + "  filter traces: " + filterTraces + "\n" + "  multi file   : " + multiFile);
+
+            this.mTargetContext = targetContext;
+            this.mReportFile = reportFile;
+            this.mReportDir = reportDir;
+            this.mFilterTraces = filterTraces;
+            this.mMultiFile = multiFile;
+        }
+
+
+    @Override
+    public void testStarted(Description description) throws Exception {
+        if(description.isTest()) {
+            checkForNewSuite(description);
+            this.mSerializer.startTag("", TAG_CASE);
+            this.mSerializer.attribute("", ATTRIBUTE_CLASS, description.getClassName());
+            this.mSerializer.attribute("", ATTRIBUTE_NAME, description.getDisplayName());
+
+            this.mTimeAlreadyWritten = false;
+            this.mTestStartTime = System.currentTimeMillis();
+        }
+    }
+
+    @Override
+    public void testFinished(Description description) throws Exception {
+
+        recordTestTime();
+        this.mSerializer.endTag("", TAG_CASE);
+        this.mSerializer.flush();
+    }
+
+
+
+    private void checkForNewSuite(Description description) throws IOException {
+        String suiteName = description.getClassName();
+        if (mCurrentSuite == null || !mCurrentSuite.equals(suiteName)) {
+            if (mCurrentSuite != null) {
+                if (mMultiFile) {
+                    close();
+                } else {
+                    mSerializer.endTag("", TAG_SUITE);
+                    mSerializer.flush();
+                }
+            }
+
+            openIfRequired(suiteName);
+            mSerializer.startTag("", TAG_SUITE);
+            mSerializer.attribute("", ATTRIBUTE_NAME, suiteName);
+            mCurrentSuite = suiteName;
+        }
+    }
+
+
+
+
+
+
+
+    @Override
+    public void testFailure(Failure failure) throws Exception{
+        addProblem(TAG_FAILURE, failure);
+    }
+
+
+    private void addProblem(String tag, Failure failure) {
+        try {
+            recordTestTime();
+            mSerializer.startTag("", tag);
+            mSerializer.attribute("", ATTRIBUTE_MESSAGE, safeMessage(failure));
+            mSerializer.attribute("", ATTRIBUTE_TYPE, failure.getClass().getName());
+            StringWriter w = new StringWriter();
+            failure.getException().printStackTrace(mFilterTraces ? new FilteringWriter(w) : new PrintWriter(w));
+            mSerializer.text(w.toString());
+            mSerializer.endTag("", tag);
+            mSerializer.flush();
+        } catch (Exception e) {
+            Log.e(LOG_TAG, safeMessage(e));
+        }
+    }
+
+
+    private void openIfRequired(String suiteName) {
+        try {
+            if (mSerializer == null) {
+                mOutputStream = openOutputStream(resolveFileName(suiteName));
+                mSerializer = Xml.newSerializer();
+                mSerializer.setOutput(mOutputStream, ENCODING_UTF_8);
+                mSerializer.startDocument(ENCODING_UTF_8, true);
+                if (!mMultiFile) {
+                    mSerializer.startTag("", TAG_SUITES);
+                }
+            }
+        } catch (IOException e) {
+            Log.e(LOG_TAG, safeMessage(e));
+            throw new RuntimeException("Unable to open serializer: " + e.getMessage(), e);
+        }
+    }
+
+
+    private String resolveFileName(String suiteName) {
+        String fileName = mReportFile;
+        if (mMultiFile) {
+            fileName = fileName.replace(TOKEN_SUITE, suiteName);
+        }
+        return fileName;
+    }
+
+
+
+    private FileOutputStream openOutputStream(String fileName) throws IOException {
+        if (mReportDir == null) {
+            Log.d(LOG_TAG, "No reportDir specified. Opening report file '" + fileName + "' in internal storage of app under test");
+            return mTargetContext.openFileOutput(fileName, Context.MODE_WORLD_READABLE);
+        } else {
+
+
+            ensureDirectoryExists(mReportDir);
+
+            File outputFile = new File(mReportDir, fileName);
+            Log.d(LOG_TAG, "Opening report file '" + outputFile.getAbsolutePath() + "'");
+            return new FileOutputStream(outputFile);
+        }
+    }
+
+
+
+    private void ensureDirectoryExists(String path) throws IOException
+    {
+        File dir = new File(path);
+        if ((!dir.isDirectory()) && (!dir.mkdirs())) {
+            String message = "Cannot create directory '" + path + "'";
+            Log.e(LOG_TAG, message);
+            throw new IOException(message);
+        }
+    }
+
+
+
+
+
+    private void recordTestTime() throws IOException {
+        if (!this.mTimeAlreadyWritten) {
+            this.mTimeAlreadyWritten = true;
+            this.mSerializer.attribute("", ATTRIBUTE_TIME, String.format(Locale.ENGLISH, "%.3f", new Object[]{Double.valueOf((System.currentTimeMillis() - this.mTestStartTime) / 1000.0D)}));
+        }
+    }
+
+    public void close() {
+        if (mSerializer != null) {
+            try {
+                // Do this just in case endTest() was not called due to a crash in native code.
+                if (TAG_CASE.equals(mSerializer.getName())) {
+                    mSerializer.endTag("", TAG_CASE);
+                }
+
+                if (mCurrentSuite != null) {
+                    mSerializer.endTag("", TAG_SUITE);
+                }
+
+                if (!mMultiFile) {
+                    mSerializer.endTag("", TAG_SUITES);
+                }
+                mSerializer.endDocument();
+                mSerializer.flush();
+                mSerializer = null;
+            } catch (IOException e) {
+                Log.e(LOG_TAG, safeMessage(e));
+            }
+        }
+
+        if (mOutputStream != null) {
+            try {
+                mOutputStream.close();
+                mOutputStream = null;
+            } catch (IOException e) {
+                Log.e(LOG_TAG, safeMessage(e));
+            }
+        }
+    }
+
+
+
+    private String safeMessage(Failure failure)
+    {
+        String message = failure.getMessage();
+        return failure.getClass().getName() + ": " + (message == null ? "<null>" : message);
+    }
+    private String safeMessage(Throwable error)
+    {
+        String message = error.getMessage();
+        return error.getClass().getName() + ": " + (message == null ? "<null>" : message);
+    }
+
+
+    private static class FilteringWriter extends PrintWriter
+    {
+        public FilteringWriter(Writer out)
+        {
+            super(out);
+        }
+
+        public void println(String s)
+        {
+            for (String filtered : JUnitReportListener.DEFAULT_TRACE_FILTERS) {
+                if (s.contains(filtered)) {
+                    return;
+                }
+            }
+
+            super.println(s);
+        }
+    }
+}
